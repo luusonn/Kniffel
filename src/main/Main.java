75661446509ec9a5159fc908868c9e3be5fbba70
oscar <oscar@Oscarrio.home>
2026-08-31 11:50:06 +0200

    suggestion of a GUI it took me way too long to figure it out.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
      
     	//new Dicecup();
+    	//new ScoreblockGUI();
     }
 }
--- src/main/ScoreblockGUI.java
+++ src/main/ScoreblockGUI.java
@@ -1,11 +1,73 @@
 package main;
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
-public class ScoreblockGUI extends JFrame {
-	
-	public ScoreblockGUI() {
-		//here is the GUI
+
+
+public class ScoreblockGUI extends JFrame{
+
+	    private JTable table;
+
+	    public ScoreblockGUI() {
+
+	        setTitle("Schreibblock");
+	        setSize(400, 500);
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setLayout(new BorderLayout());
+
+	        String[] columns = {
+	            "Kategorie",
+	            "Spieler X"
+	        };
+	        
+	        Object[][] rows = {
+	        	    {"Einser", ""},
+	        	    {"Zweier", ""},
+	        	    {"Dreier", ""},
+	        	    {"Vierer", ""},
+	        	    {"Fünfer", ""},
+	        	    {"Sechser", ""},
+	        	    {"Dreierpasch", ""},
+	        	    {"Viererpasch", ""},
+	        	    {"Full House", ""},
+	        	    {"Kleine Straße", ""},
+	        	    {"Große Straße", ""},
+	        	    {"Kniffel", ""},
+	        	    {"Chance", ""}
+	        	
+	        		
+	        };
+
+	        table = new JTable(rows, columns) {
+
+	            @Override
+	            public boolean isCellEditable(int row, int column) {
+	                return false;
+	            }
+	        };
+	        
+	        table.addMouseListener(new MouseAdapter() {
+
+	            @Override
+	            public void mouseClicked(MouseEvent e) {
+
+	                int row = table.getSelectedRow();
+
+	                System.out.println("Clicked: " + row);
+	            }
+	        });
+
+	        JScrollPane scrollPane = new JScrollPane(table);
+
+	        add(scrollPane, BorderLayout.CENTER);
+
+	        setVisible(true);
+	    }
 	}
 
-}
