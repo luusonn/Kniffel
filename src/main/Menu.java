337c4ae1da313d868eb77c2a4c7e0c67dc6b6d6b
oscar <oscar@Oscarrio.home>
2026-09-04 12:17:43 +0200

    updating the rules tab a bit

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Menu extends JFrame {
 
@@ -114,23 +115,36 @@ public class Menu extends JFrame {
 
 
         JLabel title = new JLabel("RULES");
-        JLabel rule1 = new JLabel("Feld1:(Einser) Punkte: Summe aller Einsen");
-        JLabel rule2 = new JLabel("Feld2:(Zweier) Punkte: Summe aller Zweien");
-        JLabel rule3 = new JLabel("Feld3:(Dreier) Punkte: Summe aller Dreien");
-        JLabel rule4 = new JLabel("Feld4:(Vierer) Punkte: Summe aller Vieren");
-        JLabel rule5 = new JLabel("Feld5:(Fuenfer) Punkte: Summe aller Fuenfen");
-        JLabel rule6 = new JLabel("Feld6:(Sechser) Punkte: Summe aller Sechsen");
-        JLabel rule7 = new JLabel("Feld7:(Dreierpasch) dreimal diesselbe Zahl->z.B 1+1+1+2+4=9 (Punkte: Augen aller Wuerfeln)");
-        JLabel rule8 = new JLabel("Feld8:(Viererpasch) viermal diesselbe Zahl->z.B 1+1+1+1+6=10 (Punkte: Augen aller Wuerfeln)");
-        JLabel rule9 = new JLabel("Feld9:(FullHouse) einmal zwei und einmal drei gleiche Zahlen");
-        JLabel rule10 = new JLabel("z.B 1+1+1+2+2=9 (Punkte:25 unabhaengig von der Augenzahl)");
-        JLabel rule11 = new JLabel("Feld10:(Kleine Strasse) Wuerfelkombination von 4 aufeinanderfolgenden Zahlen von 1-4 oder 2-5 oder 3-6");
-        JLabel rule12 = new JLabel("z.B 2+3+4+5 (Punkte: 40)");
-        JLabel rule13 = new JLabel("Feld11:(Grosse Strasse) Wuerfelkombination von 4 aufeinanderfolgenden Zahlen von 1-5 oder 2-6");
-        JLabel rule14 = new JLabel("z.B 1+2+3+4+5 (Punkte=40");
-        JLabel rule15 = new JLabel("Feld12:(Kniffel) fuenfmal die selbe Zahl ->z.B 3+3+3+3+3");
-        JLabel rule16 = new JLabel("Punkte: 50 für den ersten Kniffel, 100 für jeden weiteren Kniffel (kann in beliebiges Feld eintragen werden)");
-        JLabel rule17 = new JLabel("Feld13:(Chance) keine besondere Kombination (Punkte: Augen aller Wuerfel)");
+        JTextArea rule1 = new JTextArea(" Ziel: die meisten Punkte bekommen durch geschicktes Würfeln bestimmter Kombinationen"
+        		+ "\n Jeder Spieler würfelt bis zu 3 mal wobei er eine beiebige Anzahl an Würfeln beiseite legen kann um folgende Kategorien zu füllen:"
+        		+ "\n\n Feld1: (Einser) Punkte: Summe aller Einsen"
+        		+ "\n Feld2: (Zweier) Punkte: Summe aller Zweien"
+        		+ "\n Feld3: (Dreier) Punkte: Summe aller Dreien"
+        		+ "\n Feld4: (Vierer) Punkte: Summe aller Vieren"
+        		+ "\n Feld5: (Fuenfer) Punkte: Summe aller Fuenfen"
+        		+ "\n Feld6: (Sechser) Punkte: Summe aller Sechsen"
+        		+ "\n Feld7: (Dreierpasch) dreimal diesselbe Zahl->z.B 1+1+1+2+4=9 (Punkte: Augen aller Wuerfeln)"
+        		+ "\n Feld8:(Viererpasch) viermal diesselbe Zahl->z.B 1+1+1+1+6=10 (Punkte: Augen aller Wuerfeln)"
+        		+ "\n Feld9: (FullHouse) einmal zwei und einmal drei gleiche Zahlen z.B 1+1+1+2+2=9 (Punkte:25 unabhaengig von der Augenzahl)"
+        		+ "\n Feld10: (Kleine Strasse) Wuerfelkombination von 4 aufeinanderfolgenden Zahlen von 1-4 oder 2-5 oder 3-6 z.B 2+3+4+5 (Punkte: 40)\""
+        		+ "\n Feld11: (Grosse Strasse) Wuerfelkombination von 4 aufeinanderfolgenden Zahlen von 1-5 oder 2-6 z.B 1+2+3+4+5 (Punkte=40"
+        		+ "\n Feld12: (Kniffel) fuenfmal die selbe Zahl ->z.B 3+3+3+3+3 \n               Punkte: 50 für den ersten Kniffel, 100 für jeden weiteren Kniffel (kann in beliebiges Feld eintragen werden)"
+        		+ "\n Feld13: (Chance) keine besondere Kombination (Punkte: Augen aller Wuerfel)"); 
+        
+        JTextArea rule2 = new JTextArea(" nüzliche Anmerkungen zum Programm: \n"
+        		+ "\n Nach Beginn des Kniffelspiels und auswählen der Spieleranzahl seht ihr das Spiel vor euch."
+        		+ "\n\n In der linken oberen Ecke steht der momentane Spieler. Links unten der momentane Punktestand."
+        		+ "\n\n Das System würfelt einmal AUTOMATISCH. Die vorgegebenen Würfel könnt ihr also als ersten der 3 Würfe pro Runde sehen."
+        		+ "\n Unter jedem Würfel ist ein Knopf 'select', ausgewählte Würfel werden rot und werden beim nächsten würfeln NICHT mitgewürfelt."
+        		+ "\n Wenn das gewünschte Ergebnis für eine der Kategorien erreicht ist oder 3 mal gewürfelt wurde, drückt auf den 'confirm'-Button."
+        		+ "\n\n Jetzt könnt ihr eure Punkte in die Tabelle eintragen lassen."
+        		+ "\n Dazu einfach auf die Spalte drücken und dann mit dem 'Put in: ...' Knopf bestätigen."
+        		+ "\n Dein Zug ist damit beendet. Jetzt wird automatisch für den nächsten Spieler gewürfelt."
+        		+ "\n\n Wenn du keine Punkte für eine Kategorie erreicht hast kannst du diese auch streichen lassen. "
+        		+ "\n Daraufhin wird in diese Katrgorie eine Null geschrieben und sie wird nicht bewertet.");
+
+        rule1.setEditable(false);
+        rule2.setEditable(false);
 
         rulesPanel.add(title);
 
@@ -143,21 +157,6 @@ public class Menu extends JFrame {
 
         rulesPanel.add(rule1);
         rulesPanel.add(rule2);
-        rulesPanel.add(rule3);
-        rulesPanel.add(rule4);
-        rulesPanel.add(rule5);
-        rulesPanel.add(rule6);
-        rulesPanel.add(rule7);
-        rulesPanel.add(rule8);
-        rulesPanel.add(rule9);
-        rulesPanel.add(rule10);
-        rulesPanel.add(rule11);
-        rulesPanel.add(rule12);
-        rulesPanel.add(rule13);
-        rulesPanel.add(rule14);
-        rulesPanel.add(rule15);
-        rulesPanel.add(rule16);
-        rulesPanel.add(rule17);
 
 
         rulesFrame.add(rulesPanel);
